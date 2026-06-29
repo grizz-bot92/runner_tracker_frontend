1d0e5b0fbc47ad01f057bb5ce30dec945274de20
grizz-bot92 <bbenoit752@gmail.com>
2026-06-29 06:09:12 -0700

    ui cleanup and backedn connections

--- src/pages/RaceDirector.tsx
+++ src/pages/RaceDirector.tsx
@@ -10,6 +10,7 @@ import Box from '@mui/material/Box';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormGroup from '@mui/material/FormGroup';
+import { FiEdit } from "react-icons/fi";
 import "./raceDirector.css";
 
 type Race = {
@@ -24,8 +25,25 @@ type Race = {
   };
 }
 
+type Runner = {
+  name: string;
+  bib_number: number;
+}
+
+type AidStation = {
+  name: string;
+  mile_marker: string;
+  cutoff_time: {
+    hours: number;
+    minutes: number;
+  };
+  crew_access: boolean;
+}
+
 const RaceDirector = () => {
   const [race, setRace] = useState<Race[]>([]);
+  const [runner, setRunner] = useState<Runner[]>([]);
+  const [aidStation, setAidStations] = useState<AidStation[]>([]);
   const [value, setValue] = useState('1');
 
   const handleChange = (event: React.SyntheticEvent, newValue : string) => {
@@ -38,9 +56,15 @@ const RaceDirector = () => {
     .then(data => setRace(data.race));
   }, []);
 
+  useEffect(() => {
+    fetch('http://localhost:8080/runners')
+    .then(response => response.json())
+    .then(data => setRunner(data.runner))
+  }, []);
+
   return(
     <div className="page">
-      <div className="header-top">
+      <div className="header-top-race">
         <p className="race-label">Race Director</p>
         <h2 className="header-title">Olympic Mountain 100k</h2>
       </div>
@@ -79,17 +103,33 @@ const RaceDirector = () => {
             
               <TabPanel value="2">
                 <div className="runner-details">
-                <p>Runners · 3 registered </p>
-                <Box
-                  component="form"
-                  sx={{ '& > : not(style)' : {m: 1, width: '2ch'} }}
-                  noValidate
-                  autoComplete="off"
-                >
-                  <div className="addRunnerBtn">
-                    <button>Add runner</button>
-                  </div>
-                </Box>
+                  <p>Runners · 3 registered </p>
+                  <Box
+                    component="form"
+                    sx={{ '& > : not(style)' : {m: 1, width: '25ch'} }}
+                    noValidate
+                    autoComplete="off"
+                  >
+                    <TextField id="runner-name" label="Name" variant="filled"/>
+                    <TextField id="runner-number" label="Number" variant="filled"/>
+                    <div className="addRunnerBtn">
+                      <button>Add runner</button>
+                    </div>
+                  </Box>
+                </div>
+                <div className="added-runners-div">
+                    {runner.map((r) => (
+                      <div className='runner-card'>
+                        <div className='runner-info'>
+                          <span className='bib_number'>#{r.bib_number}</span>
+                          <span className='runner-name'>{r.name}</span>
+                        </div>
+                        <div className='buttons'>
+                          <button className='editBtn'><FiEdit /></button>
+                          
+                        </div>
+                      </div>
+                    ))}
                 </div>
               </TabPanel>
 
