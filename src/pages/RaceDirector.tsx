64dbd34d2c6a0262e89fa4aabd5d9595f581cf6e
grizz-bot92 <bbenoit752@gmail.com>
2026-07-03 07:25:57 -0700

    working on rd page, need to add edit btn and crew access fucntionality

--- src/pages/RaceDirector.tsx
+++ src/pages/RaceDirector.tsx
@@ -27,12 +27,13 @@ type Race = {
 }
 
 type Runner = {
-  id: number,
+  id: number;
   name: string;
   bib_number: number;
 }
 
 type AidStation = {
+  id: number;
   name: string;
   mile_marker: string;
   cutoff_time: {
@@ -48,6 +49,9 @@ const RaceDirector = () => {
   const [aidStation, setAidStation] = useState<AidStation[]>([]);
   const [runnerName, setRunnerName] = useState('');
   const [bibNumber, setBibNumber] = useState('');
+  const [aidStationName, setAidStationName] = useState('');
+  const [mileMarker, setMileMarker] = useState('');
+  const [crewAccess, setCrewAccess] = useState(false);
   const [value, setValue] = useState('1');
 
   
@@ -74,12 +78,34 @@ const RaceDirector = () => {
       name: runnerName,
       bib_number: parseInt(bibNumber),
     });
-    setRunner([...runner, result.data.runner])
+    setRunner([...runner, result.data.runner]);
   }
 
   const deleteRunner = async (id: number) => {
-    await axios.delete('http://localhost:8080/runners/${id}');
-    setRunner(runner.filter((r) => r.id !== id));
+    try{
+      await axios.delete(`http://localhost:8080/runners/${id}`);
+      setRunner(runner.filter((r) => r.id !== id));
+    } catch(e){
+      console.error(`Runner cant be deleted after check-in ${e}`)
+    }
+  }
+
+  const addAidStation = async () => {
+    const result = await axios.post('http://localhost:8080/aid_stations', {
+      name: aidStationName,
+      mile_marker: mileMarker,
+      crew_access: crewAccess,
+    });
+    setAidStation([...aidStation, result.data.aidStation]);
+  }
+
+  const deleteAidStation = async (id : number) => {
+    try {
+      await axios.delete(`http://localhost:8080/aid_stations/${id}`);
+      setAidStation(aidStation.filter((a) => a.id != id));
+    } catch(e){
+      console.error(`Aid Station not deleted ${e}`)
+    }
   }
 
   const handleChange = (event: React.SyntheticEvent, newValue : string) => {
@@ -92,6 +118,18 @@ const RaceDirector = () => {
 
   const handleBibChange = (e : React.ChangeEvent<HTMLSelectElement>) => {
     setBibNumber(e.target.value);
+  }
+
+  const handleAidStationNameChange = (e : React.ChangeEvent<HTMLSelectElement>) => {
+    setAidStationName(e.target.value);
+  }
+
+  const handleMileMarkerChange = (e : React.ChangeEvent<HTMLSelectElement>) => {
+    setMileMarker(e.target.value);
+  }
+
+  const handleCrewAccess = (e : React.ChangeEvent<HTMLSelectElement>) => {
+    setCrewAccess(e.target.value);
   }
 
   const textStyleField = {
@@ -153,7 +191,10 @@ const RaceDirector = () => {
             
               <TabPanel value="2">
                 <div className="runner-details">
-                  <p className='rd-header'>Runners · 3 registered </p>
+                  <div className='registered-header'>
+                    <p className='rd-header'>Runners ·  </p>
+                    <p className='runner-count'> {runner.length} Registered</p>
+                  </div>
                   <Box
                     component="form"
                     sx={{ '& > :not(style)' : {m: 1, width: '25ch'} }}
@@ -192,7 +233,7 @@ const RaceDirector = () => {
                         </div>
                         <div className='buttons'>
                           <FiEdit className='editBtn' style={{ color: '#B5502E', fontSize: '18px'}} />
-                          <RiDeleteBinLine className='delBtn' style={{ color: '#B5502E', fontSize: '20px' }}/>
+                          <RiDeleteBinLine className='delBtn' onClick={() => deleteRunner(r.id)} style={{ color: '#B5502E', fontSize: '20px' }}/>
                         </div>
                       </div>
                     ))}
@@ -201,7 +242,7 @@ const RaceDirector = () => {
 
             <TabPanel value="3">
                 <div className="aid-station-details">
-                <p className='rd-header'>Add Aid Station</p>
+                <p className='aid-header'>Add Aid Station</p>
                 <Box
                     component="form"
                     sx={{ '& > :not(style)' : {m: 1, width: '25ch'} }}
@@ -212,6 +253,8 @@ const RaceDirector = () => {
                       id="aid-station-name" 
                       label="Name" 
                       variant="filled"
+                      onChange={handleAidStationNameChange}
+                      value={aidStationName}
                       sx={textStyleField}
                     />
                     
@@ -219,16 +262,37 @@ const RaceDirector = () => {
                       id="mile-marker" 
                       label="Mile marker" 
                       variant="filled"
+                      onChange={handleMileMarkerChange}
+                      value={mileMarker}
                       sx={textStyleField}  
                     /> 
                     
                     <div className="addBtn">
-                      <button>Add</button>
+                      <button onClick={addAidStation}>Add</button>
                     </div>
                     <FormGroup>
                       <FormControlLabel control={<Checkbox />} label = "Crew access allowed" />
                     </FormGroup>
                   </Box>
+                </div>
+
+                <div className='added-aid-station'>
+                  <div className='aid-station-header'>
+                    <p className='aid-header'>Aid Stations ·  </p>
+                    <p className='aid-count'> {aidStation.length} on course</p>
+                  </div>
+                  {aidStation.map((a) => (
+                    <div className='aid-station-div' key={a.id}>
+                      <div className='aid-station-info'>
+                        <span className='mile-marker' style={{color: '#B5502E'}}>mi {a.mile_marker}</span>
+                        <span className='aid-station-name'>{a.name}</span>
+                      </div>
+                      <div className='buttons'>
+                        <FiEdit className='editBtn' style={{ color: '#B5502E', fontSize: '18px'}} />
+                        <RiDeleteBinLine className='delBtn' onClick={() => deleteAidStation(a.id)} style={{ color: '#B5502E', fontSize: '20px' }} />
+                      </div>
+                    </div>
+                  ))}
                 </div>
               </TabPanel>
             </TabContext>
