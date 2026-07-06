6015a895ed0d119bc0123b681e0fe205d2127be4
grizz-bot92 <bbenoit752@gmail.com>
2026-07-05 18:35:12 -0700

    race director page finished ??

--- src/pages/RaceDirector.tsx
+++ src/pages/RaceDirector.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, type ReactEventHandler } from 'react';
 import axios from 'axios';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
@@ -10,19 +10,20 @@ import Box from '@mui/material/Box';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormGroup from '@mui/material/FormGroup';
-import { FiEdit } from "react-icons/fi";
-import "./raceDirector.css";
+import { FiAlertOctagon, FiEdit } from "react-icons/fi";
 import { RiDeleteBinLine } from "react-icons/ri";
+import { RiGroupLine } from "react-icons/ri";
+import "./raceDirector.css";
 
 type Race = {
+  id: number;
   name: string;
   date: string;
-  distance: number;
+  distance: string;
   elevation_gain: number;
-  cutoff_time: {
-    hours?: number;
-    minutes?: number;
-    seconds?: number;
+  cutoff_time: string | {
+    hours: number;
+    minutes: number;
   };
 }
 
@@ -36,10 +37,6 @@ type AidStation = {
   id: number;
   name: string;
   mile_marker: string;
-  cutoff_time: {
-    hours: number;
-    minutes: number;
-  };
   crew_access: boolean;
 }
 
@@ -47,6 +44,10 @@ const RaceDirector = () => {
   const [race, setRace] = useState<Race[]>([]);
   const [runner, setRunner] = useState<Runner[]>([]);
   const [aidStation, setAidStation] = useState<AidStation[]>([]);
+  const [raceName, setRaceName] = useState('');
+  const [raceDate, setRaceDate] = useState('');
+  const [raceDistance, setRaceDistance] = useState('');
+  const [raceCutoff, setRaceCutoff] = useState('');
   const [runnerName, setRunnerName] = useState('');
   const [bibNumber, setBibNumber] = useState('');
   const [aidStationName, setAidStationName] = useState('');
@@ -58,27 +59,53 @@ const RaceDirector = () => {
   useEffect(() => {
     fetch('http://localhost:8080/races')
     .then(response => response.json())
-    .then(data => setRace(data.race));
+    .then(data => setRace(data.race.sort((a: Race, b: Race) => parseFloat(a.distance) - parseFloat(b.distance))));
   }, []);
 
   useEffect(() => {
     fetch('http://localhost:8080/runners')
     .then(response => response.json())
-    .then(data => setRunner(data.runner))
+    .then(data => setRunner(data.runner.sort((a: Runner, b: Runner) => a.bib_number - b.bib_number)));
   }, []);
 
   useEffect(() => {
     fetch('http://localhost:8080/aid_stations')
     .then(response => response.json())
-    .then(data => setAidStation(data.aid_station))
+    .then(data => setAidStation(data.aid_station.sort((a: AidStation, b: AidStation) => parseFloat(a.mile_marker) - parseFloat(b.mile_marker))))
   }, []);
 
+  const addRace = async() => {
+    await axios.post('http://localhost:8080/races', {
+      name: raceName,
+      date: new Date(raceDate),
+      distance: parseFloat(raceDistance),
+      cutoff_time: raceCutoff
+    });
+
+    const response = await fetch('http://localhost:8080/races')
+    const data = await response.json();
+
+    setRace(data.race.sort((a:Race, b: Race ) => parseFloat(a.distance) - parseFloat(b.distance)));
+  }
+
+  const deleteRace = async(id : number) => {
+    try{
+      await axios.delete(`http://localhost:8080/races/${id}`)
+      setRace(race.filter((r) => r.id != id));
+    } catch(e){
+      console.error(`Race not deleted ${e}`)
+    }    
+  }
+
   const addRunner = async () => {
-    const result = await axios.post('http://localhost:8080/runners', {
+    await axios.post('http://localhost:8080/runners', {
       name: runnerName,
       bib_number: parseInt(bibNumber),
     });
-    setRunner([...runner, result.data.runner]);
+
+    const response = await fetch('http://localhost:8080/runners')
+    const data = await response.json();
+    setRunner(data.runner.sort((a: Runner, b: Runner) => a.bib_number - b.bib_number));
   }
 
   const deleteRunner = async (id: number) => {
@@ -91,12 +118,16 @@ const RaceDirector = () => {
   }
 
   const addAidStation = async () => {
-    const result = await axios.post('http://localhost:8080/aid_stations', {
+    console.log('Add aid Station called')
+    await axios.post('http://localhost:8080/aid_stations', {
       name: aidStationName,
       mile_marker: mileMarker,
       crew_access: crewAccess,
     });
-    setAidStation([...aidStation, result.data.aidStation]);
+
+    const response = await fetch('http://localhost:8080/aid_stations');
+    const data = await response.json();
+    setAidStation(data.aid_station.sort((a: AidStation, b: AidStation) => parseFloat(a.mile_marker) - parseFloat(b.mile_marker)));
   }
 
   const deleteAidStation = async (id : number) => {
@@ -112,24 +143,44 @@ const RaceDirector = () => {
     setValue(newValue);
   };
   
-  const handleNameChange = (e : React.ChangeEvent<HTMLSelectElement>) => {
+  const handleRunnerNameChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setRunnerName(e.target.value);
   }
 
-  const handleBibChange = (e : React.ChangeEvent<HTMLSelectElement>) => {
+  const handleBibChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setBibNumber(e.target.value);
   }
 
-  const handleAidStationNameChange = (e : React.ChangeEvent<HTMLSelectElement>) => {
+  const handleRaceNameChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setRaceName(e.target.value);
+  }
+
+  const handleDateChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setRaceDate(e.target.value);
+  }
+  
+  const handleRaceCutoff = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setRaceCutoff(e.target.value);
+  }
+
+  const handleDistanceChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setRaceDistance(e.target.value);
+  }
+
+  const handleAidStationNameChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setAidStationName(e.target.value);
   }
 
-  const handleMileMarkerChange = (e : React.ChangeEvent<HTMLSelectElement>) => {
+  const handleMileMarkerChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setMileMarker(e.target.value);
   }
 
-  const handleCrewAccess = (e : React.ChangeEvent<HTMLSelectElement>) => {
-    setCrewAccess(e.target.value);
+  const handleAidCutoff = (e : React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setAidCutoff(e.target.value);
+  }
+
+  const handleCrewAccess = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCrewAccess(e.target.checked);
   }
 
   const textStyleField = {
@@ -178,14 +229,36 @@ const RaceDirector = () => {
                   noValidate
                   autoComplete="off"
                 >
-                  <TextField id="race-name" label="Race Name" variant="filled" sx={textStyleField}/>
-                  <TextField id="race-date" label="Date" variant="filled" sx={textStyleField}/>
-                  <TextField id="race-distance" label="Distance" variant="filled" sx={textStyleField}/>
-                  <TextField id="race-cutoff" label="Cutoff" variant="filled" sx={textStyleField}/>
+                  <TextField id="race-name" label="Race Name" variant="filled" sx={textStyleField} onChange={handleRaceNameChange} value={raceName}/>
+                  <TextField id="race-distance" label="Distance" variant="filled" sx={textStyleField} onChange={handleDistanceChange} value={raceDistance}/>
+                  <TextField id="race-date" label="Date" variant="filled" sx={textStyleField} onChange={handleDateChange} value={raceDate}/>
+                  <TextField id="race-cutoff" label="Cutoff" variant="filled" sx={textStyleField} onChange={handleRaceCutoff} value={raceCutoff} placeholder='e.g 30 hours'/>
                   <div className="saveBtn">
-                    <button>Save changes</button>
+                    <button onClick={addRace}>Save changes</button>
                   </div>
                 </Box>
+                </div>
+
+                <div className='added-race-div'>
+                  {race.map((r) => (
+                      <div className='race-info' key={`${r.id}`}>
+                        <div className='individual-race-info'>
+                          <div className='race-name-heading'>
+                            <span className='race-name' style={{fontWeight: 'bold', fontSize: '15px'}}>{r.name}</span>
+                          </div>
+                        <div className='race-sub-info'>
+                          <span className='race-date'> {new Date(r.date).toLocaleDateString()}</span>
+                          <span className='race-distance'> · {r.distance} mi · </span>
+                          <span className='race-cutoff'>{typeof r.cutoff_time === 'object' ? `${r.cutoff_time.hours} hr cutoff` : r.cutoff_time}</span>  
+                        </div>
+
+                        </div>
+                        <div className='buttons'>
+                            <FiEdit className='editBtn' style={{ color: '#B5502E', fontSize: '18px'}} />
+                            <RiDeleteBinLine className='delBtn' onClick={() => deleteRace(r.id)} style={{ color: '#B5502E', fontSize: '20px' }}/>
+                          </div>
+                      </div>
+                    ))}    
                 </div>
               </TabPanel>
             
@@ -205,7 +278,7 @@ const RaceDirector = () => {
                       id="runner-name" 
                       label="Name" 
                       variant="filled"
-                      onChange={handleNameChange}
+                      onChange={handleRunnerNameChange}
                       value={runnerName}
                       sx={textStyleField}    
                       />
@@ -219,7 +292,7 @@ const RaceDirector = () => {
                       />
                         
                     <div className="addRunnerBtn">
-                      <button onClick={addRunner}>Add runner</button>
+                      <button  type='button' onClick={addRunner}>Add runner</button>
                     </div>
                   </Box>
                 </div>
@@ -265,13 +338,22 @@ const RaceDirector = () => {
                       onChange={handleMileMarkerChange}
                       value={mileMarker}
                       sx={textStyleField}  
-                    /> 
+                    />
+
+                    {/* <TextField
+                      id="cutoff-time"
+                      label="Cutoff Time"
+                      variant="filled"
+                      onChange={handleAidCutoff}
+                      value={aidCutoff}
+                      sx={textStyleField}
+                    /> */}
                     
                     <div className="addBtn">
-                      <button onClick={addAidStation}>Add</button>
+                      <button type='button' onClick={addAidStation}>Add</button>
                     </div>
                     <FormGroup>
-                      <FormControlLabel control={<Checkbox />} label = "Crew access allowed" />
+                      <FormControlLabel control={<Checkbox onChange={handleCrewAccess} checked={crewAccess} />} label = "Crew access allowed" />
                     </FormGroup>
                   </Box>
                 </div>
@@ -286,6 +368,7 @@ const RaceDirector = () => {
                       <div className='aid-station-info'>
                         <span className='mile-marker' style={{color: '#B5502E'}}>mi {a.mile_marker}</span>
                         <span className='aid-station-name'>{a.name}</span>
+                        {a.crew_access && <RiGroupLine style={{ color: '#B5502E', fontSize: '16px' }} />}
                       </div>
                       <div className='buttons'>
                         <FiEdit className='editBtn' style={{ color: '#B5502E', fontSize: '18px'}} />
