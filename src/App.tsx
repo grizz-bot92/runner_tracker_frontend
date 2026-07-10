028b2d3077fff273f389125d14eba4c8abd2958f
grizz-bot92 <bbenoit752@gmail.com>
2026-07-10 10:56:45 -0700

    landing page route

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ const App = () => {
   return(
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<RunnerTracker />} />
         <Route path="/check_in" element={<VolunteerCheckIn/>}/>
         <Route path="/race_director" element={<RaceDirector/>}/>
         <Route path="/runner_tracker" element={<RunnerTracker/>}/>
