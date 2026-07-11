c194b456ce3558e5b000d894b85f45dfffd2b7f6
grizz-bot92 <bbenoit752@gmail.com>
2026-07-11 11:17:25 -0700

    installed socket client and error chekcing

--- src/socket.tsx
+++ src/socket.tsx
@@ -1,3 +1,11 @@
 import { io } from 'socket.io-client';
 
 export const socket = io(import.meta.env.VITE_API_URL as string);
+
+socket.on('connect', () => {
+  console.log('Socket connected', socket.id);
+});
+
+socket.on('connect_error', (err) => {
+  console.log('Socket connection error: ', err);
+});
