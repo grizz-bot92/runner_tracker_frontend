4cb5dc2e00d8c120c78b27ab591f0c730c34ca97
grizz-bot92 <bbenoit752@gmail.com>
2026-07-13 21:45:47 -0700

    protected routes for redirect if not valid role

--- src/components/ProtectedRoutes.tsx
+++ src/components/ProtectedRoutes.tsx
@@ -1,7 +1,7 @@
+import React from "react";
 import { Navigate } from "react-router-dom";
 
-
-const ProtectedRoutes = ({ children }) => {
+const ProtectedRoutes = ({ children }: { children: React.ReactNode }) => {
   const token = localStorage.getItem('token');
 
   if(!token){
